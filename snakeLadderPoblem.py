fd9857c09502b81292efd53f8d4157bc107b7607
bharath02 <fee0228@gmail.com>
2020-02-15 15:24:09 +0530

    Option 0 to noplay, ladder, snake bite of 3 play

--- snakeLadderPoblem.py
+++ snakeLadderPoblem.py
@@ -1,12 +1,24 @@
 """This problem simulates a Snake and Ladder Game. The Player starts from 0 rolls the die to get a number between 1 to 6, finds a safe place, ladder or a snake keeps doing till the winning spot 100 is achieved."""
 #UC1 Snake and Ladder game played with single player at start position 0
 #UC2 Player rolls the die to get a number 1 to 6
+#UC3  The Player then Check for a Option. they are No Play, Ladder or Snake.
 import random
 def snakeLader():
     player1=0
     die=throw()
-    player1+=die
-    return player1
+    # Random Used for Noplay, Ladder and Snake bite
+    option=random.randint(0,2)
+    if(option==0):
+        print("NO Play stay same Number")
+        return player1
+    elif(option==1):
+        print("You get a ladder to play increase")
+        player1+=die
+        return player1
+    else:
+        print("Snake Bite you")
+        player1 -= die
+        return player1
 
 def throw():
     n = random.randint(1, 6)
