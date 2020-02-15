06dfbb708befa148093cf5ddccfe8dcb2f8209b1
bharath02 <fee0228@gmail.com>
2020-02-15 16:22:43 +0530

    player should will play the game problem

--- snakeLadderPoblem.py
+++ snakeLadderPoblem.py
@@ -3,27 +3,32 @@
 #UC2 Player rolls the die to get a number 1 to 6
 #UC3  The Player then Check for a Option. they are No Play, Ladder or Snake.
 #UC4 Repeat till the Player reaches the winning position 100.
+#UC5position go above 100, the player stays in the same previous position till the player gets the exact number that adds to 100
+
 import random
 def snakeLader(player1):
     die=throw()
     # Random Used for Noplay, Ladder and Snake bite
-    option=random.randint(0,2)
-    if(option==0):
-        print("NO Play stay same Number")
-        return player1
-    elif(option==1):
-        print("You get a ladder to play increase")
-        player1+=die
-        return player1
-    else:
-        print("Snake Bite you")
-        player1 -= die
-        return player1
+    while(int(player1)<=100):
+        option=random.randint(0,2)
+
+        if(option==0):
+            print("NO Play stay same Number")
+            return player1
+        elif(option==1):
+            print("You get a ladder to play increase")
+            player1+=die
+            return player1
+        else:
+            print("Snake Bite you")
+            player1 -= die
+            return player1
 
 def throw():
     n = random.randint(1, 6)
     return n
 player1=0
-while(snakeLader(player1)<=100):
+while(snakeLader(player1)<101):
     player1=snakeLader(player1)
-    print("Postion of a player1",player1)
+    if(player1<=100):
+        print("Postion of a player1",player1)
