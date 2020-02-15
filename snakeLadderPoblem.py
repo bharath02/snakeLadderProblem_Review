c5137aadd7e2470227438ae862df7726ebf68b2d
bharath02 <fee0228@gmail.com>
2020-02-15 13:59:20 +0530

    installing Position of player1

---
+++ snakeLadderPoblem.py
@@ -0,0 +1,7 @@
+"""This problem simulates a Snake and Ladder Game. The Player starts from 0 rolls the die to get a number between 1 to 6, finds a safe place, ladder or a snake keeps doing till the winning spot 100 is achieved."""
+#UC1 Snake and Ladder game played with single player at start position 0
+def snakeLader(player1):
+
+    return player1
+player1=0
+print("Postion of a player1",snakeLader(player1))
